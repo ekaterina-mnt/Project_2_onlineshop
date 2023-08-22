b6c571e425662cb80cc8124c04b07bcbae76353b
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2023-08-22 12:22:04 +0300

    Method of registration

--- register_process.php
+++ register_process.php
@@ -1,5 +1,42 @@
-<p>la</p>
-
 <?php
-header('Location: /')
-?>
+$name = $_POST['name'];
+$login = $_POST['login'];
+$password = $_POST['password'];
+$password_confirm = $_POST['password_confirm'];
+
+$confirm = boolval($password === $password_confirm);
+
+$link = mysqli_connect('localhost', 'root', '', 'onlineshop');
+var_dump(mysqli_query($link, "SELECT * FROM users WHERE login='$login'")->num_rows);
+
+if (
+    empty($name) or empty($login) or
+    empty($password) or empty($password_confirm)
+) {
+    session_start();
+    $_SESSION['flash'] = 'Введены не все данные';
+    $_SESSION['post'] = $_POST;
+    header('Location: /register');
+} elseif (!$confirm) {
+    session_start();
+    $_SESSION['flash'] = 'Пароли не совпадают';
+    $_SESSION['post'] = $_POST;
+    header('Location: /register');
+} elseif (mysqli_query($link, "SELECT * FROM users WHERE login='$login'")->num_rows) {
+    session_start();
+    $_SESSION['flash'] = "Пользователь с таким логином уже существует";
+    $_SESSION['post'] = $_POST;
+    header('Location: /register');
+} else {
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    mysqli_query($link, "INSERT INTO users (name, login, password) VALUES ('$name', '$login', '$password')");
+
+    if (mysqli_query($link, "SELECT * FROM users WHERE name='$name' AND login='$login' AND password='$password'")) {
+        session_start();
+        $_SESSION['flash'] = 'Аккаунт успешно зарегистрирован!';
+        header('Location: /');
+    } else {
+        echo '<p>Возникла ошибка. Попробуйте еще раз или сообщите нам о проблеме.</p>';
+    }
+}
--- views/register.php
+++ views/register.php
@@ -1,7 +1,11 @@
 <p>
 <form action="/register_process.php" method="POST">
-    <input name="name" placeholder="Имя">
-    <input name="email" placeholder="E-mail">
+    <input name="name" type="text" placeholder="Имя" value="<?php if (isset($_SESSION['post'])) {
+                                                                echo($_SESSION['post']['name']);
+                                                            } ?>">
+    <input name="login" type="text" placeholder="Придумайте логин" value="<?php if (isset($_SESSION['post'])) {
+                                                                echo($_SESSION['post']['login']);
+                                                            } ?>">
     <input name="password" type="password" placeholder="Придумайте пароль">
     <input name="password_confirm" type="password" placeholder="Повторите пароль">
     <input type="submit" value="Зарегистрироваться">
