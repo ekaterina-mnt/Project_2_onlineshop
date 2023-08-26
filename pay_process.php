49424d70d90c0f05ae1f7ff47bd0aad5b0835f77
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2023-08-26 18:03:02 +0300

    Add pay method

--- pay_process.php
+++ pay_process.php
@@ -1,12 +1,26 @@
 <?php
 $link = mysqli_connect('localhost', 'root', '', 'onlineshop');
+session_start();
 
-$order = mysqli_query($link, "SELECT * FROM orders WHERE id=$good_id and status='in_cart'");
-if (!empty($order)) {
-    mysqli_query($link, "UPDATE orders SET status='paid'");
-    $_SESSION['flash'] = 'Товар успешно оплачен!';
+if ($_POST) {
+
+    foreach ($_POST as $good_id => $i) {
+        $order = mysqli_query($link, "SELECT * FROM orders WHERE id=$good_id and status='in_cart'");
+
+        if (!empty($order)) {
+
+            $order_id = mysqli_fetch_assoc($order)['id'];
+            mysqli_query($link, "UPDATE orders SET status='paid' WHERE id=$order_id");
+        } else {
+            $_SESSION['flash'] = 'Товар не найден в корзине!';
+            header('Location: /cart');
+        }
+    }
+
+    $_SESSION['flash'] = 'Заказ успешно оплачен!';
     header('Location: /orders');
+
 } else {
-    $_SESSION['flash'] = 'Товар не найден в корзине!';
-    header('Location: /orders');
+    $_SESSION['flash'] = 'Не выбран товар для оплаты.';
+    header('Location: /cart');
 }
--- views/cart.php
+++ views/cart.php
@@ -2,39 +2,47 @@
 $link = mysqli_connect('localhost', 'root', '', 'onlineshop');
 if ($_SESSION['auth']) {
     $user_id = $_SESSION['user_id'];
-    $id_cart = mysqli_query($link, "SELECT good_id FROM orders WHERE user_id=$user_id and status='in_cart'");
+    $orders = mysqli_query($link, "SELECT id, good_id FROM orders WHERE user_id=$user_id and status='in_cart'");
+
+    if ($orders->num_rows) {
     $goods = [];
 
-    if ($id_cart->num_rows) {
+        foreach ($orders as $order) {
+            $good_id = $order['good_id'];
+            $good = mysqli_query($link, "SELECT * FROM goods WHERE id=$good_id") or die(mysqli_error($link));
+            $goods[$order['id']] = $good;
+        }
 
-    foreach ($id_cart as $i) {
-        $id = $i['good_id'];
-        $good = mysqli_query($link, "SELECT * FROM goods WHERE id=$id") or die(mysqli_error($link));
-        $goods[] = $good;
-    }
-
-    echo '<table>';
-    foreach ($goods as $good) { 
-        $good = mysqli_fetch_assoc($good); ?>
-        <tr>
-            <td>
-                <a href="/goods/<?= $good['id'] ?>">
-                    <p><?= $good['name'] ?><br>
-                        <?= $good['price'] ?><br>
-                        <?= $good['description'] ?>
-                    </p>
-                </a>
-            </td>
-            <td>
-                <a href="/delete_from_cart/<?= $good['id'] ?>">
-                    Удалить из корзины
-                    <?php $_SESSION['cart_redirect'] = TRUE; ?>
-                </a>
-            </td>
-        </tr>
-    <?php } ?>
-    </table>
-<?php 
+?>
+        <form action="/pay_process.php" method="POST">
+            <table>
+                <?php
+                foreach ($goods as $order_id => $good) {
+                    $good = mysqli_fetch_assoc($good); ?>
+                    <tr>
+                        <td>
+                            <input type="checkbox" name="<?= $order_id ?>">
+                        </td>
+                        <td>
+                            <a href="/goods/<?= $good['id'] ?>">
+                                <p><?= $good['name'] ?><br>
+                                    <?= $good['price'] ?><br>
+                                    <?= $good['description'] ?>
+                                </p>
+                            </a>
+                        </td>
+                        <td>
+                            <a href="/delete_from_cart/<?= $good['id'] ?>">
+                                Удалить из корзины
+                                <?php $_SESSION['cart_redirect'] = TRUE; ?>
+                            </a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </table>
+            <input type="submit" value="Оплатить">
+        </form>
+<?php
     } else {
         echo "<p>Корзина пуста</p>";
     }
