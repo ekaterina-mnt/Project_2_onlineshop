58cc11950d5ba6f02ff855423077d0c168876371
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2023-08-30 21:32:51 +0300

    Change date format + fix redirect

--- add_to_cart.php
+++ add_to_cart.php
@@ -5,9 +5,9 @@ $user_id = $_SESSION['user_id'];
 $good_price = mysqli_query($link, "SELECT price FROM goods WHERE id=$good_id");
 $good_price = mysqli_fetch_assoc($good_price)['price'];
 
-$date = date("y-m-d m:i:s", time());
+$date = date("y-m-d H:i:s", time());
 
 mysqli_query($link, "INSERT INTO orders (good_id, user_id, sum, status, timestamps) VALUES ($good_id, $user_id,  '$good_price', 'in_cart', '$date')") or die(mysqli_error($link));
 
 $_SESSION['flash'] = 'Товар успешно добавлен в корзину!';
-header('Location: /goods/index');
+header('Location: /goods');
